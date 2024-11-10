66b453d019f09b4cf4b2e33a4432ea78d630c885
Francisco Sales <saneto18@hotmail.com>
2024-11-10 19:56:05 -0300

    format percentual

--- req.py
+++ req.py
@@ -3,21 +3,21 @@ import requests
 # Enviando a requisição POST para o servidor
 response = requests.post("https://frsales.pythonanywhere.com/", json={
     "arroz_inicial": 10000,
-    "arroz_final": 22000,
-    "entradas_arroz": 75000,
-    "casca_inicial": 7000,
-    "casca_final": 9000,
-    "saidas_casca": 13000,
+    "arroz_final": 10000,
+    "entradas_arroz": 65000,
+    "casca_inicial": 5000,
+    "casca_final": 5000,
+    "saidas_casca": 14000,
     "pallet_farelo_inicial": 10,
     "pallet_farelo_final": 20,
     "contagem_estoquista_farelo": 200,
-    "bag_quirela_inicial": 800,
-    "bag_quirela_final": 0,
-    "contagem_estoquista_quirela": 4800
+    "bag_quirela_inicial": 0,
+    "bag_quirela_final": 500,
+    "contagem_estoquista_quirela": 3200
 })
 
 if response.status_code == 200:
-    dados_relatorio = response.json()  # A resposta JSON
+    dados_relatorio = response.json()   # resposta JSON
 
     # Acessando o relatório como texto
     relatorio = dados_relatorio.get('relatorio', '')
--- src/main.py
+++ src/main.py
@@ -5,15 +5,15 @@ casca_gerada = casca(int(input("Casca inicial:")), int(input("Casca final:")), i
 farelo_gerado = farelo(int(input("Pallet farelo inicial:")), int(input("Pallet farelo final:")), int(input("Contagem estoquista:")))
 quirela_gerada = quirela(int(input("Bag quirela inicial:")), int(input("Bag quirela final:")), int(input("Contagem estoquista:")))
 
-percentual_casca_arroz = (casca_gerada * 100) // arroz_descascado
-percentual_farelo_arroz = (farelo_gerado * 100) // arroz_descascado
-percentual_quirela_arroz = (quirela_gerada * 100) // arroz_descascado
+percentual_casca_arroz = (casca_gerada * 100) / arroz_descascado
+percentual_farelo_arroz = (farelo_gerado * 100) / arroz_descascado
+percentual_quirela_arroz = (quirela_gerada * 100) / arroz_descascado
 
 print (f"""\n*Relatório de produção:*
 - Início do descasque: 07:15
 - Fim: 17:05
-- Observações: 
+- Observações:
 - Quantidade descascada: {f"{arroz_descascado:,}".replace(",", ".")} kg
-- Casca gerada: {f"{casca_gerada:,}".replace(",", ".")} kg ({percentual_casca_arroz}% do arroz descascado)
-- Farelo gerado: {f"{farelo_gerado:,}".replace(",", ".")} kg ({percentual_farelo_arroz}% do arroz descascado)
-- Quirela gerada: {f"{quirela_gerada:,}".replace(",", ".")} kg ({percentual_quirela_arroz}% do arroz descascado)\n""")
+- Casca gerada: {f"{casca_gerada:,}".replace(",", ".")} kg ({"{:.2f}".format(percentual_casca_arroz)}% do arroz descascado)
+- Farelo gerado: {f"{farelo_gerado:,}".replace(",", ".")} kg ({"{:.2f}".format(percentual_farelo_arroz)}% do arroz descascado)
+- Quirela gerada: {f"{quirela_gerada:,}".replace(",", ".")} kg ({"{:.2f}".format(percentual_quirela_arroz)}% do arroz descascado)\n""")
